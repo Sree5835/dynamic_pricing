5e958329cf41056b15ba0fa03bc425b2154bda76
Sree S <sanakkayala12@gmail.com>
2024-02-11 23:34:30 +0000

    #4; push data to db complete

--- src/dynamic_pricing/db/db_init.py
+++ src/dynamic_pricing/db/db_init.py
@@ -1,114 +1,130 @@
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 import psycopg2
 import os
 
 
-load_dotenv(find_dotenv())
+def connect_to_database() -> psycopg2.extensions.connection:
+    """Connect to the PostgreSQL database."""
+    load_dotenv()
+    db_url = os.getenv("DB_URL")
+    if db_url is None:
+        raise ValueError("DB_URL environment variable is not set")
 
-url = os.environ.get("DB_URL")
-# Connect to an existing database
-conn = psycopg2.connect(url, sslmode="require")
-
-# Open a cursor to perform database operations
-cur = conn.cursor()
-
-cur.execute(
-    """
-    DROP TABLE IF EXISTS customers CASCADE;
-    DROP TABLE IF EXISTS modifiers CASCADE;
-    DROP TABLE IF EXISTS products CASCADE;
-    DROP TABLE IF EXISTS items CASCADE;
-    DROP TABLE IF EXISTS partners CASCADE;
-    DROP TABLE IF EXISTS orders CASCADE;
-    DROP TABLE IF EXISTS order_items CASCADE;
-    DROP TABLE IF EXISTS order_item_modifiers CASCADE;
-    """
-)
-
-cur.execute(
-    """CREATE TABLE customers (
-    customer_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255),
-    contact_number VARCHAR(20),
-    contact_access_code VARCHAR(20)
-);"""
-)
-
-cur.execute(
-    """CREATE TABLE partners (
-    partner_id SERIAL PRIMARY KEY,
-    partner_name VARCHAR(255) UNIQUE NOT NULL
-);"""
-)
+    return psycopg2.connect(db_url, sslmode="require")
 
 
-cur.execute(
-    """INSERT INTO partners (partner_name) VALUES (%s)""", (os.environ.get("partner1"),)
-)
-cur.execute(
-    """INSERT INTO partners (partner_name) VALUES (%s)""", (os.environ.get("partner2"),)
-)
+def create_tables(connection: psycopg2.extensions.connection) -> None:
+    """Create tables in the PostgreSQL database."""
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            DROP TABLE IF EXISTS order_item_modifiers CASCADE;
+            DROP TABLE IF EXISTS order_items CASCADE;
+            DROP TABLE IF EXISTS orders CASCADE;
+            DROP TABLE IF EXISTS partners CASCADE;
+            DROP TABLE IF EXISTS items CASCADE;
+            DROP TABLE IF EXISTS modifiers CASCADE;
+            DROP TABLE IF EXISTS customers CASCADE;
+            """
+        )
 
-cur.execute(
-    """CREATE TABLE modifiers (
-    modifier_id SERIAL PRIMARY KEY,
-    deliveroo_modifier_id VARCHAR(255) NOT NULL,
-    modifier_name VARCHAR(255) NOT NULL,
-    modifier_operational_name VARCHAR(255) NOT NULL);
-    """
-)
+        cursor.execute(
+            """CREATE TABLE customers (
+            customer_id SERIAL PRIMARY KEY,
+            first_name VARCHAR(255),
+            contact_number VARCHAR(20) UNIQUE NOT NULL,
+            contact_access_code VARCHAR(20)
+            );"""
+        )
 
-cur.execute(
-    """CREATE TABLE items (
-    item_id SERIAL PRIMARY KEY,
-    deliveroo_item_id VARCHAR(255) NOT NULL,
-    item_name VARCHAR(255) NOT NULL,
-    item_operational_name VARCHAR(255) NOT NULL);
-"""
-)
+        cursor.execute(
+            """CREATE TABLE partners (
+            partner_id SERIAL PRIMARY KEY,
+            partner_name VARCHAR(255) UNIQUE NOT NULL
+            );"""
+        )
+
+        cursor.execute(
+            """INSERT INTO partners (partner_name) VALUES (%s)""",
+            (os.getenv("PARTNER1"),),
+        )
+        cursor.execute(
+            """INSERT INTO partners (partner_name) VALUES (%s)""",
+            (os.getenv("PARTNER2"),),
+        )
+
+        cursor.execute(
+            """CREATE TABLE modifiers (
+            modifier_id SERIAL PRIMARY KEY,
+            deliveroo_modifier_id VARCHAR(255) UNIQUE NOT NULL,
+            modifier_name VARCHAR(255) NOT NULL,
+            modifier_operational_name VARCHAR(255) NOT NULL
+            );"""
+        )
+
+        cursor.execute(
+            """CREATE TABLE items (
+            item_id SERIAL PRIMARY KEY,
+            deliveroo_item_id VARCHAR(255) UNIQUE NOT NULL,
+            item_name VARCHAR(255) NOT NULL,
+            item_operational_name VARCHAR(255) NOT NULL
+            );"""
+        )
+
+        cursor.execute(
+            """CREATE TABLE orders (
+            order_id SERIAL PRIMARY KEY,
+            deliveroo_order_id VARCHAR(255) UNIQUE NOT NULL,
+            deliveroo_order_number BIGINT NOT NULL,
+            order_status VARCHAR(255) NOT NULL,
+            order_placed_timestamp TIMESTAMP NOT NULL,
+            order_updated_timestamp TIMESTAMP,
+            customer_id INT NOT NULL,
+            CONSTRAINT fk_orders_customer_id FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE SET NULL,
+            partner_id INT NOT NULL,
+            CONSTRAINT fk_orders_partner_id FOREIGN KEY (partner_id) REFERENCES partners(partner_id) ON DELETE SET NULL
+            );"""
+        )
+
+        cursor.execute(
+            """CREATE TABLE order_items (
+            order_id INT NOT NULL,
+            CONSTRAINT fk_order_items_order_id FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE SET NULL,
+            item_id INT NOT NULL,
+            CONSTRAINT fk_order_items_item_id FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE SET NULL,
+            quantity INT NOT NULL,
+            fractional_price INT NOT NULL,
+            PRIMARY KEY (order_id, item_id)
+            );"""
+        )
+
+        cursor.execute(
+            """CREATE TABLE order_item_modifiers (
+            order_id INT NOT NULL,
+            CONSTRAINT fk_order_item_modifiers_order_id FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE SET NULL,
+            item_id INT NOT NULL,
+            CONSTRAINT fk_order_item_modifiers_item_id FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE SET NULL,
+            modifier_id INT NOT NULL,
+            CONSTRAINT fk_order_item_modifiers_modifier_id FOREIGN KEY (modifier_id) REFERENCES modifiers(modifier_id) ON DELETE SET NULL,
+            quantity INT NOT NULL,
+            fractional_price INT NOT NULL,
+            PRIMARY KEY (order_id, item_id, modifier_id)
+            );"""
+        )
+
+        connection.commit()
 
 
-cur.execute(
-    """CREATE TABLE orders (
-    order_id SERIAL PRIMARY KEY,
-    deliveroo_order_id VARCHAR(255) NOT NULL,
-    deliveroo_order_number BIGINT NOT NULL,
-    order_status VARCHAR(255) NOT NULL,
-    order_placed_timestamp TIMESTAMP NOT NULL,
-    order_updated_timestamp TIMESTAMP,
-    customer_id INT NOT NULL,
-        CONSTRAINT fk_orders_customer_id FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE SET NULL,
-    partner_id INT NOT NULL,
-        CONSTRAINT fk_orders_partner_id FOREIGN KEY (partner_id) REFERENCES partners(partner_id) ON DELETE SET NULL
-);"""
-)
-cur.execute(
-    """CREATE TABLE order_items (
-    order_id INT NOT NULL,
-        CONSTRAINT fk_order_items_order_id FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE SET NULL,
-    item_id INT NOT NULL,
-        CONSTRAINT fk_order_items_item_id FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE SET NULL,
-    quantity INT NOT NULL,
-    fractional_price INT NOT NULL,
-    PRIMARY KEY (order_id, item_id)
-);"""
-)
+def main() -> None:
+    """Main function."""
+    try:
+        connection = connect_to_database()
+        create_tables(connection)
+        if "connection" in locals():
+            connection.close()
+    except Exception as e:
+        print(f"Error: {e}")
 
-cur.execute(
-    """CREATE TABLE order_item_modifiers (
-    order_id INT NOT NULL,
-        CONSTRAINT fk_order_item_modifiers_order_id FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE SET NULL,
-    item_id INT NOT NULL,
-        CONSTRAINT fk_order_item_modifiers_item_id FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE SET NULL,
-    modifier_id INT NOT NULL,
-        CONSTRAINT fk_order_item_modifiers_modifier_id FOREIGN KEY (modifier_id) REFERENCES modifiers(modifier_id) ON DELETE SET NULL,
-    quantity INT NOT NULL,
-    fractional_price INT NOT NULL,
-    PRIMARY KEY (order_id, item_id,modifier_id)
-);"""
-)
 
-conn.commit()
-
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    main()
--- src/dynamic_pricing/db/db_utils.py
+++ src/dynamic_pricing/db/db_utils.py
@@ -1,184 +1,191 @@
 from datetime import datetime
 import json
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, session, sessionmaker
 import os
 
 # Define the database connection URL
 db_url = os.environ.get("DB_URL")
 engine = create_engine(db_url, echo=True)
 
+Session = sessionmaker(bind=engine)
 
-def upsert(table_name, data_dict, pk_cols):
-    with Session(engine) as session:
-        try:
 
-            # Create a temporary table to hold the data
-            tmp_table_name = f"tmp_{table_name}"
-            values = ", ".join([f":{col}" for col in data_dict.keys()])
+def create_tmp_table(table_name, data_dict):
+    """Create a temporary table to hold the data."""
+    tmp_table_name = f"tmp_{table_name}"
+    with Session() as session:
+        drop_table = text(f"DROP TABLE IF EXISTS {tmp_table_name} CASCADE;")
+        create_table = text(
+            f"""CREATE TABLE {tmp_table_name} (LIKE {table_name} INCLUDING ALL);"""
+        )
+        insert_data = text(
+            f"INSERT INTO {tmp_table_name} ({', '.join(data_dict.keys())}) VALUES ({', '.join([':' + col for col in data_dict.keys()])})"
+        )
+        session.execute(drop_table)
+        session.execute(create_table)
+        session.execute(insert_data, data_dict)
 
-            temp_query = text(
-                f"""DROP TABLE IF EXISTS {tmp_table_name} CASCADE;
-                CREATE TABLE {tmp_table_name} (LIKE {table_name} INCLUDING ALL);
-                INSERT INTO {tmp_table_name} ({', '.join(data_dict.keys())}) VALUES ({values})"""
-            )
-            session.execute(temp_query, data_dict)
-            session.commit()
+        session.commit()
+    return tmp_table_name
 
-            # Create a list of the columns to be updated
+
+def drop_tmp_table(tmp_table_name):
+    """Drop the temporary table."""
+    with Session() as session:
+        session.execute(text(f"DROP TABLE IF EXISTS {tmp_table_name} CASCADE;"))
+        session.commit()
+
+
+def upsert(table_name, data_dict, pk_cols, returning_col=None):
+    """Upsert data into the main table."""
+    tmp_table_name = create_tmp_table(table_name, data_dict=data_dict)
+    try:
+        with Session() as session:
             conflict_cols = [
                 f"{col}=EXCLUDED.{col}"
                 for col in data_dict.keys()
                 if col not in pk_cols
             ]
 
-            # Create the upsert query
-            upsert_query = text(
-                f"""
+            query = f"""
                 INSERT INTO {table_name} ({', '.join(data_dict.keys())})
-                SELECT * FROM {tmp_table_name}
+                SELECT {', '.join(data_dict.keys())} FROM {tmp_table_name}
                 ON CONFLICT ({', '.join(pk_cols)}) DO UPDATE
-                SET {', '.join(conflict_cols)}
-                RETURNING ({', '.join(pk_cols)});
-                """
-            )
+                SET {', '.join(conflict_cols)}"""
+            query += f" RETURNING {returning_col};" if returning_col else ";"
+
+            upsert_query = text(query)
             data_dict["table_name"] = table_name
 
-            # Execute the query
             result = session.execute(upsert_query, data_dict)
-            result = result.scalar()
+            if returning_col:
+                result = result.scalar()
             session.commit()
-
-        except Exception as e:
-            # sourcery skip: raise-specific-error
-            raise Exception(f"Error upserting data into table {table_name}: {e}") from e
-
-        finally:
-            # Drop the temporary table
-            session.execute(text(f"DROP TABLE IF EXISTS {tmp_table_name} CASCADE;"))
-            session.commit()
-
+    except Exception as e:
+        raise Exception(f"Error upserting data into table {table_name}: {e}") from e
+    finally:
+        drop_tmp_table(tmp_table_name)
     return result
 
 
-def insert_webhook_order(partner_name, order_data):
-    with Session(engine) as session:
-        # Customer
-        customer_data = order_data["customer"]
-        customer_query = text(
-            "INSERT INTO customers (first_name, contact_number, contact_access_code) "
-            "VALUES (:first_name, :contact_number, :contact_access_code) RETURNING customer_id"
-        )
-        result = session.execute(
-            customer_query,
-            {
-                "first_name": customer_data["first_name"],
-                "contact_number": customer_data["contact_number"],
-                "contact_access_code": customer_data["contact_access_code"],
-            },
-        )
-        customer_id = result.scalar()
+def insert_customer(customer_data: dict) -> int:
+    """Insert or update customer data."""
+    filtered_customer_data = {
+        "first_name": customer_data["first_name"],
+        "contact_number": customer_data["contact_number"],
+        "contact_access_code": customer_data["contact_access_code"],
+    }
+    return upsert(
+        "customers", filtered_customer_data, ["contact_number"], "customer_id"
+    )
 
-        # Order
-        order_query = text(
-            "INSERT INTO orders (deliveroo_order_id, deliveroo_order_number, order_status, "
-            "order_placed_timestamp, order_updated_timestamp, customer_id, partner_id) VALUES "
-            "(:deliveroo_order_id, :deliveroo_order_number, :order_status, :order_placed_timestamp, :order_updated_timestamp,"
-            ":customer_id, :partner_id) RETURNING order_id"
-        )
-        result = session.execute(
-            order_query,
-            {
-                "deliveroo_order_id": order_data["id"],
-                "deliveroo_order_number": order_data["order_number"],
-                "order_status": order_data["status"],
-                "order_placed_timestamp": datetime.strptime(
-                    order_data["status_log"][0]["at"], "%Y-%m-%dT%H:%M:%SZ"
-                ),
-                "order_updated_timestamp": datetime.strptime(
-                    order_data["status_log"][1]["at"].split(".")[0] + "Z",
-                    "%Y-%m-%dT%H:%M:%SZ",
-                ),
-                "customer_id": customer_id,
-                "partner_id": order_data["location_id"],
-            },
-        )
-        order_id = result.scalar()
 
-        # Order Items
-        for item_data in order_data["items"]:
-            item_query = text(
-                "INSERT INTO items (deliveroo_item_id, item_name, item_operational_name) "
-                "VALUES (:deliveroo_item_id, :item_name, :item_operational_name) RETURNING item_id"
-            )
-            result = session.execute(
-                item_query,
-                {
-                    "deliveroo_item_id": item_data["pos_item_id"],
-                    "item_name": item_data["name"],
-                    "item_operational_name": item_data["operational_name"],
-                },
-            )
-            item_id = result.scalar()
+def insert_order(order_data: dict, customer_id: int) -> int:
+    """Insert or update order data."""
+    filtered_order_data = {
+        "deliveroo_order_id": order_data["id"],
+        "deliveroo_order_number": order_data["order_number"],
+        "order_status": order_data["status"],
+        "order_placed_timestamp": datetime.strptime(
+            order_data["status_log"][0]["at"], "%Y-%m-%dT%H:%M:%SZ"
+        ),
+        "order_updated_timestamp": datetime.strptime(
+            order_data["status_log"][1]["at"].split(".")[0] + "Z",
+            "%Y-%m-%dT%H:%M:%SZ",
+        ),
+        "customer_id": customer_id,
+        "partner_id": order_data["location_id"],
+    }
+    return upsert("orders", filtered_order_data, ["deliveroo_order_id"], "order_id")
 
-            order_item_query = text(
-                "INSERT INTO order_items (order_id, item_id, quantity, fractional_price) "
-                "VALUES (:order_id, :item_id, :quantity, :fractional_price)"
-            )
-            session.execute(
-                order_item_query,
-                {
-                    "order_id": order_id,
-                    "item_id": item_id,
-                    "quantity": item_data["quantity"],
-                    "fractional_price": item_data["total_price"]["fractional"],
-                },
-            )
 
-            # Order Item Modifiers
-            for modifier_data in item_data["modifiers"]:
-                modifier_query = text(
-                    "INSERT INTO modifiers (deliveroo_modifier_id, modifier_name, modifier_operational_name) "
-                    "VALUES (:deliveroo_modifier_id, :modifier_name, :modifier_operational_name) "
-                    "RETURNING modifier_id"
-                )
-                result = session.execute(
-                    modifier_query,
-                    {
-                        "deliveroo_modifier_id": modifier_data["id"],
-                        "modifier_name": modifier_data["name"],
-                        "modifier_operational_name": modifier_data["operational_name"],
-                    },
-                )
-                modifier_id = result.scalar()
+def insert_item(item_data: dict) -> int:
+    """Insert or update item data."""
+    filtered_item_data = {
+        "deliveroo_item_id": item_data["pos_item_id"],
+        "item_name": item_data["name"],
+        "item_operational_name": item_data["operational_name"],
+    }
+    return upsert(
+        "items",
+        filtered_item_data,
+        ["deliveroo_item_id"],
+        "item_id",
+    )
 
-                order_item_modifier_query = text(
-                    "INSERT INTO order_item_modifiers (order_id, item_id, modifier_id, quantity, fractional_price) "
-                    "VALUES (:order_id, :item_id, :modifier_id, :quantity, :fractional_price)"
-                )
-                session.execute(
-                    order_item_modifier_query,
-                    {
-                        "order_id": order_id,
-                        "item_id": item_id,
-                        "modifier_id": modifier_id,
-                        "quantity": modifier_data["quantity"],
-                        "fractional_price": modifier_data["total_price"]["fractional"],
-                    },
-                )
 
-        session.commit()
+def insert_modifier(modifier_data: dict) -> int:
+    """Insert or update modifier data."""
+    filtered_modifier_data = {
+        "deliveroo_modifier_id": modifier_data["id"],
+        "modifier_name": modifier_data["name"],
+        "modifier_operational_name": modifier_data["operational_name"],
+    }
+    return upsert(
+        "modifiers",
+        modifier_data,
+        ["deliveroo_modifier_id"],
+        "modifier_id",
+    )
+
+
+def insert_order_item(order_id: int, item_id: int, item_data: dict) -> None:
+    """Insert order item data."""
+    order_item_data = {
+        "order_id": order_id,
+        "item_id": item_id,
+        "quantity": item_data["quantity"],
+        "fractional_price": item_data["total_price"]["fractional"],
+    }
+    upsert(
+        table_name="order_items",
+        data_dict=order_item_data,
+        pk_cols=["order_id", "item_id"],
+    )
+
+
+def insert_order_item_modifier(
+    order_id: int, item_id: int, modifier_data: dict
+) -> None:
+    """Insert order item modifier data."""
+    order_item_modifier_data = {
+        "order_id": order_id,
+        "item_id": item_id,
+        "modifier_id": modifier_data["modifier_id"],
+        "quantity": modifier_data["quantity"],
+        "fractional_price": modifier_data["total_price"]["fractional"],
+    }
+    upsert(
+        table_name="order_item_modifiers",
+        data_dict=order_item_modifier_data,
+        pk_cols=["order_id", "item_id", "modifier_id"],
+    )
+
+
+def insert_webhook_order(partner_name: str, order_data: dict) -> None:
+    """Insert webhook order into the database."""
+    customer_id = insert_customer(order_data["customer"])
+    order_id = insert_order(order_data, customer_id)
+
+    for item_data in order_data["items"]:
+        item_id = insert_item(item_data)
+        insert_order_item(order_id, item_id, item_data)
+
+        for modifier_data in item_data["modifiers"]:
+            modifier_id = insert_modifier(modifier_data)
+            insert_order_item_modifier(order_id, item_id, modifier_data)
 
 
 if __name__ == "__main__":
     # Load the JSON data from your provided input
-    with open("./src/dynamic_pricing/db/sample.json", "r") as file:
+    with open("./src/data/webhook/sample.json", "r") as file:
         input_json = file.read()
 
     # Parse the JSON data
     order_data = json.loads(input_json)
 
-    print(order_data["body"]["order"].keys())
+    # print(order_data["body"]["order"].keys())
 
     # Insert the data into the PostgreSQL database
     insert_webhook_order("Nostimo", order_data["body"]["order"])
