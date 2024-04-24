2d747496a61a46b5d34873453562d681f297b1d3
Sree S <sanakkayala12@gmail.com>
2024-04-24 13:06:49 +0100

    config update

--- src/dynamic_pricing/webhook/config.py
+++ src/dynamic_pricing/webhook/config.py
@@ -1,5 +1,4 @@
-"""URLs for the Deliveroo API.""
-
+"""URLs for the Deliveroo API."""
 
 BASE_URL_PROD = "https://api.developers.deliveroo.com/order/v1/orders"
 BASE_URL_DEV = "https://api-sandbox.developers.deliveroo.com/order/v1/orders"
